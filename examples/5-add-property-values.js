2ad1ef3d2e2129ccd2229f17286241040e07b201
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-03-19 09:32:57 +0300

    add 5 examples

---
+++ examples/5-add-property-values.js
@@ -0,0 +1,15 @@
+// Add Property Values
+
+const myCity = {
+    city: 'Moscow',
+}
+
+console.log(myCity)
+
+myCity.popular = false
+
+console.log(myCity)
+
+myCity.country = 'Russia'
+
+console.log(myCity)
