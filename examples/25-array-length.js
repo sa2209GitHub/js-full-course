75ab504e67381fe64aea85137f57a1703038fbc7
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-03-22 19:16:53 +0300

    example 26 added

--- examples/25-array-length.js
+++ examples/25-array-length.js
@@ -8,3 +8,14 @@ console.log('myArray:', myArray, 'myArray.length:', myArray.length)
 
 myArray[4] = true
 console.log('myArray:', myArray, 'myArray.length:', myArray.length)
+
+// <2 empty items>
+myArray[7] = true
+console.log('myArray:', myArray, 'myArray.length:', myArray.length)
+
+// <1 empty item>
+myArray[6] = false
+console.log('myArray:', myArray, 'myArray.length:', myArray.length)
+
+myArray[5] = false
+console.log('myArray:', myArray, 'myArray.length:', myArray.length)
